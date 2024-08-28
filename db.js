e58a97066c13bab99083e33c53294ed0c8c917f7
REBECCANI <beckynishimwe@example.com>
2024-08-28 19:43:48 +0200

    DATABASE CONNECTION LIMIT UPDATE

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
-    connectionLimit: 10 // Set to the maximum number of connections you want to allow
+    connectionLimit: 15
 });
 
 pool.getConnection((err, connection) => {
