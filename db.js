678f5c4ed6177310ff49c813c565872be85df502
REBECCANI <beckynishimwe@example.com>
2024-08-28 19:32:41 +0200

    DATABASE

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+console.log("DB_HOST:", process.env.DB_HOST);  // Add this to debug
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -7,7 +12,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
-    connectionLimit: 5 // Set to the maximum number of connections you want to allow
+    connectionLimit: 10 // Set to the maximum number of connections you want to allow
 });
 
 pool.getConnection((err, connection) => {
