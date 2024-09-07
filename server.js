838531e6117b99ee11715fec92bdec1a83444af1
REBECCANI <beckynishimwe@example.com>
2024-09-07 10:08:55 +0200

    changes on cors

--- server.js
+++ server.js
@@ -9,13 +9,28 @@ const db = require("./db");
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-app.use(cors({
-    origin: 'https://lumina-frontend-bbf9.vercel.app',
+const allowedOrigins = [
+    'https://lumina-frontend-bbf9.vercel.app',
+    'https://your-new-domain.com', // Add your new Namecheap domain here
+    'http://localhost:3000' // Optionally allow localhost for development
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+       var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+       return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-}));
+  }));
+
 
 app.use(express.json());
 app.use(bodyParser.json());
